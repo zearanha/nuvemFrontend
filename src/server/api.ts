6ba9c037e77ff32d95ad7127baeb26967dd30a6a
zearanha <JAranha21@gmail.com>
2025-11-24 21:04:23 -0300

    adicionando back

--- src/server/api.ts
+++ src/server/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Em desenvolvimento usamos o prefixo `/api` e configuramos um proxy no Vite
 // (`vite.config.ts`) que encaminha `/api/*` para o backend remoto.
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://kong-b97fc7d5c3uskyjyt.kongcloud.dev/estoque",
   headers: {
     "Content-Type": "application/json",
   },
